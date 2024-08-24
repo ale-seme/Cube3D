41a574a50f40da4e3aa3b1de289e606db78defd7
ale-seme <alexei.semeraro@gmail.com>
2024-08-24 13:38:29 +0200

    playing with mlx42 still

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include "cub3D.h"
 #include  "fcntl.h"
 #include <limits.h>
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 320
+#include <math.h>
+
+# define SCREEN_WIDTH 1920
+# define SCREEN_HEIGHT 1000
+# define CELL_SIZE 32
+# define FOW 66
 
 
 int main(int argc, char **argv)
@@ -20,11 +24,17 @@ int main(int argc, char **argv)
     mlx = mlx_init(SCREEN_WIDTH, SCREEN_HEIGHT, "Cub3D", true);
     mlx_set_setting(MLX_STRETCH_IMAGE, true);
     
-    mlx_image_t *img = mlx_new_image(mlx, 640, 320);
+    mlx_image_t *img = mlx_new_image(mlx, SCREEN_WIDTH, SCREEN_HEIGHT);
+    int player_pixel_x = game.camera_start_info->x * CELL_SIZE + CELL_SIZE/2;
+    int player_pixel_y = game.camera_start_info->y * CELL_SIZE + CELL_SIZE/2;
+    printf("hello: %d, %d\n", game.camera_start_info->x, game.camera_start_info->y);
     {
-        mlx_put_pixel(img, game.camera_start_info->x * (640/game.n_columns), game.camera_start_info->y *(640/game.n_rows), 0x00FF00FF);
+        mlx_put_pixel(img, player_pixel_x, player_pixel_y, 0x00FF00FF);
     }
-
+    for(int f = 0; f < SCREEN_HEIGHT; f++)
+    {
+        mlx_put_pixel(img, SCREEN_WIDTH -1, f, 0xFF0000FF);
+    }
     mlx_image_to_window(mlx, img, 0, 0);
     mlx_loop(mlx);
 	// int y = -1;
